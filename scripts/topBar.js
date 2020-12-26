cb4774dede880935e727427b11fa8989789a5de7
Alex Kutz <AlexKon4@outlook.com>
2020-12-26 19:05:12 +0200

    headerBarBg Fix scroll

--- scripts/topBar.js
+++ scripts/topBar.js
@@ -1,13 +1,16 @@
 var header = $('.top-bar'),
   scrollPrev = 0;
+var headerBg = $('.top-bar__background')
 
 $(window).scroll(function() {
   var scrolled = $(window).scrollTop();
  
   if ( scrolled > 100 && scrolled > scrollPrev ) {
     header.addClass('out');
+    headerBg.addClass('out')
   } else {
     header.removeClass('out');
+    headerBg.removeClass('out')
   }
   scrollPrev = scrolled;
 });
